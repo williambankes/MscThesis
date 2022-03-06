14948053e6843564deaad889bb0dc58943a58490
williambankes <williamjamesbankes@gmail.com>
2022-03-06 21:32:47 +0000

    Added plot density image function to plot non-contour flow density

--- NormalisingFlows/utils.py
+++ NormalisingFlows/utils.py
@@ -27,3 +27,18 @@ def plot_density_contours(dense_func, title, colour='Blues'):
     con = axs.contourf(XX, YY, ZZ, cmap=colour, vmin=ZZ.min(), vmax=ZZ.max())    
     #fig.colorbar(con, ax=axs)
     axs.set_title(title)
+    
+def plot_density_image(dense_func, title, colour='Blues'):
+    
+    fig, axs = plt.subplots(figsize=(7,7))
+       
+    #create grid:    
+    XX, YY = np.meshgrid(np.linspace(-5, 5, 101), np.linspace(-5, 5, 100))
+
+    #Get input from density eval
+    py = dense_func(torch.FloatTensor(np.stack((XX.ravel(), YY.ravel())).T))
+
+    #Reshape into 2d output
+    ZZ = py.reshape(XX.shape)
+        
+    axs.imshow(ZZ)
