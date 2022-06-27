98077e189638e7bf021db7e99dde1387d6e7259f
williambankes <williamjamesbankes@gmail.com>
2022-06-27 15:41:54 +0100

    Added SequentialFlows and further testing to Projection1D

--- AutoEncodedFlows/models.py
+++ AutoEncodedFlows/models.py
@@ -13,6 +13,34 @@ import torch.nn as nn
 from torchdyn.core import NeuralODE
 
 
+class SequentialFlow(nn.Sequential):
+    
+    def __init__(self, *args):
+        """
+        Extension of the nn.Sequential class to allow for an inverse of each 
+        Module to be called in reverse order
+
+        Parameters
+        ----------
+        *args : 
+            
+        Returns
+        -------
+        None.
+
+        """        
+
+        super().__init__(*args)
+        
+        #Could try to ensure that .inverse method must exist
+        
+        
+    def inverse(self, input):
+        
+        for module in self[::-1]:            
+            input = module.inverse(input)
+        return input
+
 class Projection1D(nn.Module):
     
     def __init__(self, dims_in, dims_out, trainable=False):
@@ -54,7 +82,10 @@ class Projection1D(nn.Module):
         if trainable: self.projection = nn.parameter.Parameter(projection_matrix)
         else:         self.projection = nn.parameter.Parameter(projection_matrix, requires_grad=False)   
         
-        self.trainable = trainable
+        self.trainable, self.dims_in, self.dims_out = trainable, dims_in, dims_out
+        self._str = "Projection1D(dims_in={}, dims_out={}, trainable={})".\
+            format(self.dims_in, self.dims_out, self.trainable)
+            
         
     def forward(self, x):
         return x @ self.projection
@@ -65,7 +96,42 @@ class Projection1D(nn.Module):
         else:              pseudo_inverse = self.projection.T
             
         return x @ pseudo_inverse
+        
+    def __repr__(self):
+        return self._str
+
+    def __str__(self):
+        return self._str
     
+class NeuralODEWrapper(nn.Module):
+    
+    def __init__(self, grad_net, **ode_args):
+        
+        """
+        Wrapper around NeuralODE to implement <inverse> method and remove time
+        evals from solution
+
+        Parameters
+        ----------
+        grad_net : nn.Module
+            nn.Module that defines the vector field of the NeuralODE
+        **ode_args: dict
+            Torchdyn ode solver args (see documentation)
+
+        Returns
+        -------
+        None.
+        
+        """
+
+        super().__init__()
+        self.node = NeuralODE(grad_net, return_t_eval=False, **ode_args)
+        
+    def forward(self, x):
+        return self.node(x, t_span=torch.tensor([0.,1.]))[-1]
+
+    def inverse(self, x):
+        return self.node(x, t_span=torch.tensor([1.,0.]))[-1]
     
 class GradientNetwork(nn.Module):
     
@@ -113,35 +179,64 @@ class CNFAutoEncoderSCurve(nn.Module):
         
         #encoder layers
         grad_net_3_encode = GradientNetwork(3, 16)
-        grad_net_1_encode = GradientNetwork(1, 8)
+        grad_net_2_encode = GradientNetwork(2, 8)
         
         self.node_3_encode = NeuralODE(grad_net_3_encode, **ode_solver_args)
-        self.projection_encode = Projection1D(3,1)
-        self.node_1_encode = NeuralODE(grad_net_1_encode, **ode_solver_args)
+        self.projection_encode = Projection1D(3,2)
+        self.node_2_encode = NeuralODE(grad_net_2_encode, **ode_solver_args)
         
         #decoder layers
         grad_net_3_decode = GradientNetwork(3, 16)
-        grad_net_1_decode = GradientNetwork(1, 8)
+        grad_net_2_decode = GradientNetwork(2, 8)
         
         self.node_3_decode = NeuralODE(grad_net_3_decode, **ode_solver_args)
-        self.projection_decode = Projection1D(1, 3)
-        self.node_1_decode = NeuralODE(grad_net_1_decode, **ode_solver_args)
-
+        self.projection_decode = Projection1D(2, 3)
+        self.node_2_decode = NeuralODE(grad_net_2_decode, **ode_solver_args)
 
     def encode(self, x):
         
         _, x = self.node_3_encode(x)
         x    = self.projection_encode(x[-1])
-        _, x = self.node_1_encode(x)
+        _, x = self.node_2_encode(x)
         
         return x[-1]
 
     def decode(self, x):
-        _, x = self.node_1_decode(x)
+        _, x = self.node_2_decode(x)
         x    = self.projection_decode(x[-1])
         _, x = self.node_3_decode(x)
 
         return x[-1]
+    
+    
+class CNFAutoEncoderFlowSCurve(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+        ode_solver_args = {'sensitivity':'adjoint',
+                           'solver':'dopri5',
+                           'atol':1e-4,
+                           'rtol':1e-4}
+        
+        #encoder layers
+        grad_net_3 = GradientNetwork(3, 16)
+        grad_net_2 = GradientNetwork(2, 8)
+        
+        
+        self.flow = SequentialFlow(
+            NeuralODEWrapper(grad_net_3, **ode_solver_args),
+            Projection1D(3,2),
+            NeuralODEWrapper(grad_net_2, **ode_solver_args))
+
+        
+    def encode(self, x):
+        return self.flow(x)
+    
+    def decode(self, x):
+        return self.flow.inverse(x)
+    
+    
 
 if __name__ == '__main__':
     
--- AutoEncodedFlows/tests/models_tests.py
+++ AutoEncodedFlows/tests/models_tests.py
@@ -5,8 +5,10 @@ Created on Thu Jun 23 16:05:31 2022
 @author: William
 """
 
+import torch
 import unittest
-from AutoEncodedFlows.models import Projection1D
+from AutoEncodedFlows.models import Projection1D, SequentialFlow
+from AutoEncodedFlows.models import CNFAutoEncoderFlowSCurve
 
 
 class Projection1D_Tests(unittest.TestCase):
@@ -20,7 +22,37 @@ class Projection1D_Tests(unittest.TestCase):
         self.assertTrue(proj_layer.projection.shape == (3,2))       
         
         
+class SequentialFlow_Tests(unittest.TestCase):
+    
+    
+    def test_sequentialflow(self):
+        
+        testflow = SequentialFlow(Projection1D(2,3),
+                                  Projection1D(3,4))
+        
+        input_data = torch.ones((2,2))        
+        output_data = testflow(input_data)
+        inverse_data = testflow.inverse(output_data)
+        
+        self.assertTrue(output_data.shape == torch.Size([2,4]))
+        self.assertTrue((inverse_data == input_data).all())
+        
+class CNFAutoEncoderFlowSCurve_Tests(unittest.TestCase):
+    
+    def test_encode_decode(self):
+        
+        model = CNFAutoEncoderFlowSCurve()
+        test_data = torch.randn((10,3))
+        
+        enc_data = model.encode(test_data)
+        dec_data = model.decode(enc_data)
+        
+
+                
 if __name__ == '__main__':
+    
+    import os
+    os.chdir(b"""C:\Users\William\Documents\UCL\Modules\MSc Thesis\MscThesis""")
         
     unittest.main()
     
