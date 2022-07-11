056e866d5659c7ee0463e24dba029e9287598416
williambankes <williamjamesbankes@gmail.com>
2022-07-11 14:53:08 +0100

    Split models into modules with nn.module methods

--- AutoEncodedFlows/models.py
+++ AutoEncodedFlows/models.py
@@ -4,147 +4,23 @@ Created on Thu Jun 23 14:02:07 2022
 
 @author: William
 """
-
 import torch
 import torch.nn as nn
+from torchdyn.nn   import DepthCat
 
-from torchdyn.core import NeuralODE
+from AutoEncodedFlows.modules import NeuralODEWrapper, Projection1D
+from AutoEncodedFlows.modules import SequentialFlow
 
 
-class SequentialFlow(nn.Sequential):
-    
-    def __init__(self, *args):
-        """
-        Extension of the nn.Sequential class to allow for an inverse of each 
-        Module to be called in reverse order
-
-        Parameters
-        ----------
-        *args : 
-            
-        Returns
-        -------
-        None.
-
-        """        
-
-        super().__init__(*args)
-        
-        #Could try to ensure that .inverse method must exist
-        
-        
-    def inverse(self, input):
-        
-        for module in self[::-1]:            
-            input = module.inverse(input)
-        return input
-
-class Projection1D(nn.Module):
-    
-    def __init__(self, dims_in, dims_out, rotation=False, trainable=False):
-        """
-        Projection from lower to higher dimensional space. Inverse is calculated
-        using the pseudo inverse. Designed for data of dimensions (-1, D) where
-        D is the dims_in.
-        
-        -> rotation: https://math.stackexchange.com/questions/2369940/parametric-representation-of-orthogonal-matrices
-        i.e. parameterise a rotational matrix via matrix exponential of skew symmetric matrix
-        
-        Parameters
-        ----------
-        dims_in : int
-            Dimensionality of input data 
-        dims_out : int
-            Dimensionality of output data
-        trainable : bool, optional
-            If True the projection matrix can be learnt via backprop, if False 
-            the projection corresponds to padding the input data with zeros.
-            The default is False.
-
-        Returns
-        -------
-        None.
-        """        
-        
-        super().__init__()
-        
-        assert dims_in != dims_out,\
-            'Projection1D: dims_in and dims_out should be different sizes'
-            
-        if dims_in < dims_out:
-            projection_matrix = torch.concat([torch.eye(dims_in),
-                                              torch.zeros((dims_in, dims_out - dims_in))],
-                                             axis=-1)
-        else:
-            projection_matrix = torch.concat([torch.eye(dims_out),
-                                              torch.zeros((dims_out, dims_in - dims_out))],
-                                             axis=-1).T
-        
-        if rotation: projection_matrix 
-        
-        
-        if trainable: self.projection = nn.parameter.Parameter(projection_matrix)
-        else:         self.projection = nn.parameter.Parameter(projection_matrix, requires_grad=False)   
-        
-        
-        self.trainable, self.dims_in, self.dims_out = trainable, dims_in, dims_out
-        self._str = "Projection1D(dims_in={}, dims_out={}, trainable={})".\
-            format(self.dims_in, self.dims_out, self.trainable)
-            
-        
-    def forward(self, x):
-        return x @ self.projection
-    
-    def inverse(self, x):
-        
-        if self.trainable: pseudo_inverse = torch.linalg.pinv(self.projection)
-        else:              pseudo_inverse = self.projection.T
-            
-        return x @ pseudo_inverse
-        
-    def __repr__(self):
-        return self._str
-
-    def __str__(self):
-        return self._str
-    
-class NeuralODEWrapper(nn.Module):
-    
-    def __init__(self, grad_net, **ode_args):
-        
-        """
-        Wrapper around NeuralODE to implement <inverse> method and remove time
-        evals from solution
-
-        Parameters
-        ----------
-        grad_net : nn.Module
-            nn.Module that defines the vector field of the NeuralODE
-        **ode_args: dict
-            Torchdyn ode solver args (see documentation)
-
-        Returns
-        -------
-        None.
-        
-        """
-
-        super().__init__()
-        self.node = NeuralODE(grad_net, return_t_eval=False, **ode_args)
-        
-    def forward(self, x):
-        return self.node(x, t_span=torch.tensor([0.,1.]))[-1]
-
-    def inverse(self, x):
-        return self.node(x, t_span=torch.tensor([1.,0.]))[-1]
-    
 class GradientNetwork(nn.Module):
     
-    def __init__(self, dims, hidden_dim=32):
+    def __init__(self, dims, time_grad=False, hidden_dim=32):
         """
         Simple Network that defines the vector field of the ODE
         
-        #Add time and more complexity
+        -> Add time and more complexity
+        
+        -> Why doesn't dynamic layers work?
 
         Parameters
         ----------
@@ -161,14 +37,62 @@ class GradientNetwork(nn.Module):
         
         super().__init__()
         
-        self.network = nn.Sequential(
-                    nn.Linear(dims, hidden_dim),
-                    nn.BatchNorm1d(hidden_dim),
-                    nn.LeakyReLU(0.01),
-                    nn.Linear(hidden_dim, hidden_dim),
-                    nn.BatchNorm1d(hidden_dim),
-                    nn.LeakyReLU(0.01),
-                    nn.Linear(hidden_dim, dims))
+        """
+        #Create init layer:
+        layers = self._create_layer(dims, hidden_dim, time_grad)
+        
+        #Create layers:
+        for l in range(n_layers):
+            layers.extend(self._create_layer(hidden_dim, hidden_dim, time_grad))
+        
+        #final layer:
+        if time_grad: layers.extend([nn.DepthCat(1), nn.Linear(hidden_dim + 1, dims)])
+        else:         layers.append(nn.Linear(hidden_dim, dims))
+        
+        self.network = nn.Sequential(*layers)        
+        
+        """
+        if time_grad:
+            self.network = nn.Sequential(
+                        DepthCat(1),
+                        nn.Linear(dims + 1, hidden_dim),
+                        nn.BatchNorm1d(hidden_dim),
+                        nn.LeakyReLU(0.01),
+                        DepthCat(1),
+                        nn.Linear(hidden_dim + 1, hidden_dim),
+                        nn.BatchNorm1d(hidden_dim),
+                        nn.LeakyReLU(0.01),
+                        DepthCat(1),
+                        nn.Linear(hidden_dim + 1, hidden_dim),
+                        nn.BatchNorm1d(hidden_dim),
+                        nn.LeakyReLU(0.01),
+                        DepthCat(1),
+                        nn.Linear(hidden_dim + 1, dims))
+        else:
+            self.network = nn.Sequential(
+                        nn.Linear(dims, hidden_dim),
+                        nn.BatchNorm1d(hidden_dim),
+                        nn.LeakyReLU(0.01),
+                        nn.Linear(hidden_dim, hidden_dim),
+                        nn.BatchNorm1d(hidden_dim),
+                        nn.LeakyReLU(0.01),
+                        nn.Linear(hidden_dim, hidden_dim),
+                        nn.BatchNorm1d(hidden_dim),
+                        nn.LeakyReLU(0.01),
+                        nn.Linear(hidden_dim, dims))
+
+        
+    def _create_layer(self, dims_in, dims_out, time_grad):
+        
+        if time_grad: layer = [DepthCat(1),
+                               nn.Linear(dims_in + 1, dims_out),
+                               nn.BatchNorm1d(dims_out),
+                               nn.LeakyReLU(0.01)]
+        else:         layer = [nn.Linear(dims_in, dims_out),
+                               nn.BatchNorm1d(dims_out),
+                               nn.LeakyReLU(0.01)]   
+        
+        return layer
         
     def forward(self, x):
         return self.network(x)        
@@ -176,34 +100,45 @@ class GradientNetwork(nn.Module):
 
 class CNFAutoEncoderSCurve(nn.Module):
     
-    def __init__(self):
+    def __init__(self, trainable=False, orthogonal=False, time_grad=False,
+                 hidden_dim_state=16, hidden_dim_latent=8,
+                 t_span=torch.tensor([0.,1.])):
         super().__init__()
         
+        #change args to speed up network?
         ode_solver_args = {'sensitivity':'adjoint',
                            'solver':'dopri5',
                            'atol':1e-4,
                            'rtol':1e-4}
         
         #encoder layers
-        grad_net_3_encode = GradientNetwork(3, 16)
-        grad_net_2_encode = GradientNetwork(2, 8)
-        
+        grad_net_3_encode = GradientNetwork(dims=3, hidden_dim=hidden_dim_state,
+                                            time_grad=time_grad)
+        grad_net_2_encode = GradientNetwork(dims=2, hidden_dim=hidden_dim_latent,
+                                            time_grad=time_grad)
+                
         self.encode_flow = nn.Sequential(
-                            NeuralODEWrapper(grad_net_3_encode, **ode_solver_args),
-                            Projection1D(3,2, trainable=True),
-                            NeuralODEWrapper(grad_net_2_encode, **ode_solver_args))
+                            NeuralODEWrapper(grad_net_3_encode, t_span=t_span,
+                                             **ode_solver_args),
+                            Projection1D(3,2, trainable=trainable, 
+                                             orthog=orthogonal),
+                            NeuralODEWrapper(grad_net_2_encode, t_span=t_span,
+                                             **ode_solver_args))
         
         #decoder layers
         grad_net_3_decode = GradientNetwork(3, 16)
         grad_net_2_decode = GradientNetwork(2, 8)
         
         self.decode_flow = nn.Sequential(
-                            NeuralODEWrapper(grad_net_2_decode, **ode_solver_args),
-                            Projection1D(2,3, trainable=True),
-                            NeuralODEWrapper(grad_net_3_decode, **ode_solver_args))
+                            NeuralODEWrapper(grad_net_2_decode, t_span=t_span,
+                                             **ode_solver_args),
+                            Projection1D(2,3, trainable=trainable, 
+                                             orthog=orthogonal),
+                            NeuralODEWrapper(grad_net_3_decode, t_span=t_span,
+                                             **ode_solver_args))
         
     def encode(self, x):
-        
+                
         return self.encode_flow(x)
 
     def decode(self, x):
---
+++ AutoEncodedFlows/modules.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 13:30:12 2022
+
+@author: William
+"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+from torchdyn.core import NeuralODE
+
+class SequentialFlow(nn.Sequential):
+    
+    def __init__(self, *args):
+        """
+        Extension of the nn.Sequential class to allow for an inverse of each 
+        Module to be called in reverse order
+
+        Parameters
+        ----------
+        *args : 
+            
+        Returns
+        -------
+        None.
+
+        """        
+
+        super().__init__(*args)
+        
+        #Could try to ensure that .inverse method must exist
+        
+        
+    def inverse(self, input):
+        
+        for module in self[::-1]:            
+            input = module.inverse(input)
+        return input
+    
+    
+class Projection1D(nn.Module):
+    
+    def __init__(self, dims_in, dims_out, orthog=False, trainable=False):
+        """
+        Projection from lower to higher dimensional space. Inverse is calculated
+        using the pseudo inverse. Designed for data of dimensions (-1, D) where
+        D is the dims_in.
+        
+        -> rotation: https://math.stackexchange.com/questions/2369940/parametric-representation-of-orthogonal-matrices
+        i.e. parameterise a rotational matrix via matrix exponential of skew symmetric matrix
+        
+        Parameters
+        ----------
+        dims_in : int
+            Dimensionality of input data 
+        dims_out : int
+            Dimensionality of output data
+        orthog: bool, optional -> not quite the right term...
+            If True the 'identity' projection matrix is preceeded by an orthogonal
+            projection mapping with determinant 1.        
+        trainable : bool, optional
+            If True the projection matrix can be learnt via backprop, if False 
+            the projection corresponds to torch.eye padded or sliced.
+            The default is False.
+
+        Returns
+        -------
+        None.
+        """        
+        
+        super().__init__()
+        
+        assert dims_in != dims_out,\
+            'Projection1D: dims_in and dims_out should be different sizes'
+        assert not trainable or not orthog,\
+            'Projection1D: rotation and trainable should not be both True'
+            
+        #Create Projection matrix:
+        if dims_in < dims_out:
+            projection_matrix = torch.concat([torch.eye(dims_in),
+                                              torch.zeros((dims_in, dims_out - dims_in))],
+                                             axis=-1)
+        else:
+            projection_matrix = torch.concat([torch.eye(dims_out),
+                                              torch.zeros((dims_out, dims_in - dims_out))],
+                                             axis=-1).T
+                
+        if orthog: 
+            self.rotations = nn.parameter.Parameter(torch.randn((dims_in - 1)))
+            self.indices   = np.array([(i+1, i) for i in range(dims_in - 1)]) 
+            
+        if trainable: self.projection = nn.parameter.Parameter(projection_matrix)
+        else:         self.projection = nn.parameter.Parameter(projection_matrix, requires_grad=False)   
+        
+        self.trainable, self.orthog = trainable, orthog
+        self.dims_in, self.dims_out = dims_in, dims_out
+        self._str = "Projection1D(dims_in={}, dims_out={}, orthog={}, trainable={})".\
+            format(self.dims_in, self.dims_out, self.orthog, self.trainable)
+                
+    def orthogonal_matrix(self):
+        
+        tri_matrix = torch.zeros((self.dims_in, self.dims_in), 
+                                 device=self.rotations.device)
+        tri_matrix[self.indices[:,0], self.indices[:,1]] = self.rotations
+        skew_matrix = tri_matrix - tri_matrix.T        
+        
+        return torch.matrix_exp(skew_matrix)
+        
+    def forward(self, x):
+        
+        if self.orthog: return x @ self.orthogonal_matrix() @ self.projection
+        else: return x @ self.projection
+    
+    def inverse(self, x):
+                
+        if self.trainable: return x @ torch.linalg.pinv(self.projection)
+        elif self.orthog:  return x @ torch.linalg.pinv(
+                self.orthogonal_matrix() @ self.projection)
+        else: return x @ self.projection.T
+        
+        
+    def __repr__(self):
+        return self._str
+
+    def __str__(self):
+        return self._str
+    
+    
+class NeuralODEWrapper(nn.Module):
+    
+    def __init__(self, grad_net, t_span=torch.tensor([0., 1.]), **ode_args):
+        
+        """
+        Wrapper around NeuralODE to implement <inverse> method and remove time
+        evals from solution
+
+        Parameters
+        ----------
+        grad_net : nn.Module
+            nn.Module that defines the vector field of the NeuralODE
+        **ode_args: dict
+            Torchdyn ode solver args (see documentation)
+
+        Returns
+        -------
+        None.
+        
+        """
+
+        super().__init__()
+        self.node = NeuralODE(grad_net, return_t_eval=False, **ode_args)
+        self.t_span = t_span
+        
+    def forward(self, x):
+        return self.node(x, t_span=self.t_span)[-1]
+
+    def inverse(self, x):
+        return self.node(x, t_span=self.t_span)[-1]
