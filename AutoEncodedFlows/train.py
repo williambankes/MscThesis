04029facb0b77de52d08e8ef382ebd6d4c874d20
williambankes <williamjamesbankes@gmail.com>
2022-07-11 14:53:33 +0100

    try finish loop to control wandb connection

--- AutoEncodedFlows/train.py
+++ AutoEncodedFlows/train.py
@@ -14,7 +14,6 @@ import wandb
 
 import torch
 import torch.nn as nn
-import torch.utils.data as data
 
 
 class AutoEncoder(pl.LightningModule):
@@ -52,14 +51,12 @@ class AutoEncoder(pl.LightningModule):
     def training_step(self, batch, batch_idx):
                 
         x = batch
-        
+                
         encoded = self.model.encode(x)
         decoded = self.model.decode(encoded)
         
         #reconstruction loss:
         loss = nn.MSELoss()(decoded, x)
-        
-        #clever logging setup in the training step
         
         #Log and return metrics:
         wandb.log({'training loss': loss.detach().item()})
@@ -75,21 +72,30 @@ if __name__ == '__main__':
     
     from AutoEncodedFlows.models import CNFAutoEncoderSCurve, CNFAutoEncoderFlowSCurve
     from AutoEncodedFlows.datasets import SCurveDataset
-    from AutoEncodedFlows.utils import Experiment, wandb_3d_point_cloud, wandb_3d_point_cloud_scurveAE
+    from AutoEncodedFlows.utils.experiments import Experiment, get_experiment_notes
+    from AutoEncodedFlows.utils.analysis import wandb_3d_point_cloud, wandb_3d_point_cloud_scurveAE
+    from AutoEncodedFlows.utils.analysis import plotly_3d_point_cloud_scurveAE, plotly_latent_space_scurveAE
     
     torch.set_num_threads(14)
     
     trainer_args = {'gpus':1,
-                    'max_epochs':20,
+                    'max_epochs':100,
                     'enable_checkpointing':False}
-    model_args = {}
+    model_args = {'trainable':False,
+                  'orthogonal':True,
+                  'time_grad':True,
+                  'hidden_dim_state':32,
+                  'hidden_dim_latent':16,
+                  't_span':torch.tensor([0.,2.])}
     dataset_args = {'n_samples':10_000}
     dataloader_args = {'batch_size':508,
                        'shuffle':True}
     
+    
+    notes = get_experiment_notes()
     exp1 = Experiment(project='AutoEncodingFlows',
-                      notes='lr=0.001',
-                      tags=['MscThesis'],
+                      notes=notes,
+                      tags=['MscThesis', 'AutoEncoder'],
                       learner=AutoEncoder,
                       model=CNFAutoEncoderSCurve,
                       dataset=SCurveDataset,
@@ -97,26 +103,15 @@ if __name__ == '__main__':
                       model_args=model_args,
                       dataset_args=dataset_args,
                       dataloader_args=dataloader_args)
-    
 
-    exp1.run()
-    exp1.analyse([wandb_3d_point_cloud, wandb_3d_point_cloud_scurveAE])
-    exp1.finish()
-    
-    """    
-    
-    exp2 = Experiment(project='AutoEncodingFlows',
-                      notes='Testing Experiment Class',
-                      tags=['MscThesis'],
-                      learner=AutoEncoder,
-                      model=CNFAutoEncoderFlowSCurve,
-                      dataset=SCurveDataset,
-                      trainer_args=trainer_args,
-                      model_args=model_args,
-                      dataset_args=dataset_args,
-                      dataloader_args=dataloader_args)
-    
-    exp2.run()
-    exp2.analyse([wandb_3d_point_cloud])
-    exp2.finish()
-    """
+    try:
+        exp1.run()
+        exp1.wandb_analyse([wandb_3d_point_cloud, wandb_3d_point_cloud_scurveAE])
+        exp1.analyse([plotly_3d_point_cloud_scurveAE, plotly_latent_space_scurveAE])
+        exp1.finish()
+    finally:
+        exp1.finish()
+        
+
+#%%
+exp1.analyse([plotly_3d_point_cloud_scurveAE, plotly_latent_space_scurveAE])
