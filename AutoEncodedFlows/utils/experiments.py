e754a8a993bc7f174f4c0c3878566a70c4e7daea
williambankes <williamjamesbankes@gmail.com>
2022-07-19 12:48:16 +0100

    reworked experiment setup

--- AutoEncodedFlows/utils/experiments.py
+++ AutoEncodedFlows/utils/experiments.py
@@ -11,62 +11,157 @@ import torch.utils.data as data
 
 class Experiment:
     
-    def __init__(self, project, notes, tags, learner, model, dataset, 
-                 trainer_args, model_args, dataset_args, dataloader_args):
+    def __init__(self, project, tags, learner, model, dataset, 
+                 trainer_args, learner_args, model_args, dataset_args,
+                 dataloader_args):
+        """
+        A class to manage wandb api interactions and pytorch lightning training
+        interactions. Input relevant models and parameters then run the .run()
+        method to run pytorch lightning model training.
+
+
+        Parameters
+        ----------
+        project : str
+            str with the wandb project name 
+        tags : list()
+            A list of short descriptors for each experiment
+        learner : <pytorchlightning.LightningModule>
+            A LightningModule implementation that manages the training of the
+            model
+        model : <pytorch.nn.Module>
+            The pytorch model that is given as an argument to the trainer. 
+        dataset : <pytorch.utils.data.Dataset>
+            Dataset on which the model training is run
+        <param>_args : dict
+            Dictionary of arguments to be passed to the relevant parameter. These
+            are also logged in the wandb config so should be a python primative.
+
+        Returns
+        -------
+        None.
+
+        """
         
+    
         #Setup configs:
         configs = {'Learner': learner.__name__,
                    'Model'  : model.__name__,
                    'Dataset': dataset.__name__}
         configs.update(model_args)
+        configs.update(learner_args)
         configs.update(trainer_args)
         configs.update(dataset_args)
         configs.update(dataloader_args)
         
         #May restructure depending on project scope:
-        self.experiment_name = "{}_{}".format(configs['Model'],
-                                              configs['Dataset'])
+        self.group_name = "{}_{}_{}".format(configs['Learner'],
+                                            configs['Model'],
+                                            configs['Dataset'])
+        self.experiment_name = get_user_input("Enter Experiment Name:")
+        notes = get_user_input("Enter notes on experiment {}:".\
+                               format(self.experiment_name))
         
-        print('Creating Experiment:{}'.format(self.experiment_name))
+        print('Creating Experiment:{} in group: {}'.format(self.experiment_name,
+                                                           self.group_name))
+
         #Setup model and trainer:
         self.runner = wandb.init(
                         project=project,
+                        group=self.group_name,
                         name=self.experiment_name,
                         notes=notes,
                         tags=tags,
                         config=configs)
                 
         self.model = model(**model_args)
-        self.learner = learner(self.model) #Can add learner_args...
+        self.learner = learner(self.model, **learner_args)
         self.trainer = pl.Trainer(**trainer_args)
         self.dataloader = data.DataLoader(dataset(**dataset_args),
                                           **dataloader_args)
+        self.fitted = False
                     
     def run(self):
+        """
+        Run the trainer .fit() method
+
+        Returns
+        -------
+        None.
+
+        """
         
         self.trainer.fit(self.learner, train_dataloaders=self.dataloader)
+        self.fitted = True
         
             
     def wandb_analyse(self, analyse_funcs):
+        """
+        Run analytics functions custom to an experiment. The outputs are expected
+        to be wandb logs and are processed and passed to the wandb logger
+        process
+
+        Parameters
+        ----------
+        analyse_funcs : <function>(model:pl.LightningModule,
+                                 dataloader:pytorch.utils.data.Dataloader)
+            Custom analytics function of suitable format that analyses and 
+            returns a wandb log.
+        
+        Returns
+        -------
+        None.
+
+        """
         
         for i, func in enumerate(analyse_funcs):
             output = func(self.trainer.model, self.dataloader)
             self.runner.log(output)
             
     def analyse(self, analyse_funcs):
+        """
+        Run general analytics functions on model and dataset.
         
+        Parameters
+        ----------
+        analyse_funcs : <function>(model:pl.LightningModule,
+                                   dataloader:pytorch.utils.data.Dataloader)
+            Custom analytics function
+
+        Returns
+        -------
+        None.
+
+        """
         for i, func in enumerate(analyse_funcs):
             output = func(self.trainer.model, self.dataloader)
             
     def finish(self):
+        """
+        Ensures the wandb run is finished. More functionality can be added
+        as necessary...
+
+        Returns
+        -------
+        None.
+
+        """
         print('Experiment:{} finishing'.format(self.experiment_name))
         self.runner.finish()
         
         
-def get_experiment_notes():
+def get_user_input(query):
+    """
+    Get experiment note string via input query
+
+    Returns
+    -------
+    notes : str
+
+    """
     
-    notes = input("Experiment Note:")
-    
-    #assertions here
+    notes = input(query)
+
+    assert isinstance(notes, str), ""    
     
     return notes
