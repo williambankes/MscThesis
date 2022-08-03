b679e5e12cedb078ca9f7e2a25d75826a25d1eea
williambankes <williamjamesbankes@gmail.com>
2022-08-03 14:34:01 +0100

    Restructured test script

--- AutoEncodedFlows/manifold_models.py
+++ AutoEncodedFlows/manifold_models.py
@@ -5,9 +5,126 @@ Created on Wed Aug  3 11:51:03 2022
 @author: William
 """
 
+import wandb
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import MultivariateNormal
+from torchdyn.core import NeuralODE
+from torchdyn.models import CNF, hutch_trace
+from torchdyn.nn import Augmenter, DepthCat
+import pytorch_lightning as pl
 from AutoEncodedFlows.modules import MADE
+from typing import Union, Callable
+
+class CNFLearner(pl.LightningModule):
+    
+    def __init__(self, vector_field:nn.Module, dims:int):
+        """
+        Learner setup for Torchdyn CNF model
+
+        Parameters
+        ----------
+        model : nn.Module
+            AutoEncoder model with encode and decode methods
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        super().__init__()
+        self.__name__ = 'CNFLearner'
+        self.iters = 0
+        self.dims = dims
+        self.losses = list()
+        
+        #Define model parameters:
+        ode_solver_args = {'solver':'tsit5'}
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        
+        #Create Jacobian noise dist and base dist:
+        self.mean = torch.zeros(self.dims).to(device)
+        self.cov = torch.eye(self.dims).to(device)
+        self.base_dist = MultivariateNormal(self.mean, self.cov)
+        
+        #Create model:
+        cnf = CNF(vector_field,
+                  noise_dist=self.base_dist,
+                  trace_estimator=hutch_trace)
+        node = NeuralODE(cnf, **ode_solver_args)
+        self.model = nn.Sequential(Augmenter(augment_idx=1, augment_dims=1),
+                                   node)
+        wandb.watch(self.model)
+        
+        
+    def forward(self, x):
+        
+        #Set model time span forward:
+        self.model[1].t_span = torch.linspace(0, 1, 10)
+        return self.model(x)
+    
+    def training_step(self, batch, batch_idx):
+        self.iters += 1 
+        t_eval, xtrJ = self.model(batch)
+        xtrJ = xtrJ[-1] #select the end point of the trajectory:
+        logprob = self.base_dist.log_prob(xtrJ[:,1:]).to(batch) - xtrJ[:,0]
+        loss = -torch.mean(logprob)
+        
+        if self.current_epoch % 10 == 0:
+            self.losses.append(loss.cpu().detach())
+        
+        #wandb logging:
+        wandb.log({'training loss': loss.detach().item(),
+                   'epoch': self.current_epoch})
+           
+        return {'loss': loss}   
+            
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=2e-3, weight_decay=1e-5)
+
+class VectorFieldNoTime(nn.Module):
+    
+    def __init__(self, dims, hidden_dims):
+        
+        super().__init__()
+        
+        self.__name__ = 'cnf_vector_field'
+        self.network = nn.Sequential(
+                           nn.Linear(dims, hidden_dims),
+                           nn.Tanh(),
+                           nn.Linear(hidden_dims, hidden_dims),
+                           nn.Tanh(),
+                           nn.Linear(hidden_dims, hidden_dims),
+                           nn.Tanh(),
+                           nn.Linear(hidden_dims, dims))
+    
+    def forward(self, x):
+        return self.network(x)
+    
+class VectorFieldTime(nn.Module):
+    
+    def __init__(self, dims, hidden_dims):
+        
+        super().__init__()
+        
+        self.__name__ ='cnf_vector_field_w_time'
+        self.network = nn.Sequential(
+                           DepthCat(1),
+                           nn.Linear(dims + 1, hidden_dims),
+                           nn.Tanh(),
+                           DepthCat(1),
+                           nn.Linear(hidden_dims + 1, hidden_dims),
+                           nn.Tanh(),
+                           DepthCat(1),
+                           nn.Linear(hidden_dims + 1, hidden_dims),
+                           nn.Tanh(),
+                           nn.Linear(hidden_dims, dims))
+        
+    def forward(self, x):
+        return self.network(x)
+
 
 class VectorFieldMasked(nn.Module):
     
@@ -57,6 +174,38 @@ class VectorFieldMasked(nn.Module):
         trace_element = self.params * x
         
         return pointwise_element + inner_element + trace_element
+
+ 
+def MaskedTrace(model):
+    return model.params.sum(0)
+
+class MaskedCNF(nn.Module):
+    def __init__(self, net:nn.Module, trace_estimator:Union[Callable, None]=None, noise_dist=None, order=1):
+        """Continuous Normalizing Flow
+        :param net: function parametrizing the datasets vector field.
+        :type net: nn.Module
+        :param trace_estimator: specifies the strategy to otbain Jacobian traces. Options: (autograd_trace, hutch_trace)
+        :type trace_estimator: Callable
+        :param noise_dist: distribution of noise vectors sampled for stochastic trace estimators. Needs to have a `.sample` method.
+        :type noise_dist: torch.distributions.Distribution
+        :param order: specifies parameters of the Neural DE.
+        :type order: int
+        """
+        super().__init__()
+        self.net, self.order = net, order # order at the CNF level will be merged with DEFunc
+        self.trace_estimator = MaskedTrace
+
+    def forward(self, x):
+        with torch.set_grad_enabled(True):
+            # first dimension is reserved to divergence propagation
+            x_in = x[:,1:].requires_grad_(True)
+
+            # the neural network will handle the datasets-dynamics here
+            if self.order > 1: x_out = self.higher_order(x_in)
+            else: x_out = self.net(x_in)
+
+            trJ = self.trace_estimator(self.net)
+        return torch.cat([-trJ[:, None], x_out], 1) + 0*x 
 
 
 if __name__ == '__main__':
--- AutoEncodedFlows/scripts/manifolds.py
+++ AutoEncodedFlows/scripts/manifolds.py
@@ -4,128 +4,13 @@ Created on Fri Jul 15 15:12:10 2022
 
 @author: William
 """
-import wandb
+
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.distributions import MultivariateNormal
-from torchdyn.core import NeuralODE
-from torchdyn.models import CNF, hutch_trace
-from torchdyn.nn import Augmenter, DepthCat
-import pytorch_lightning as pl
 from AutoEncodedFlows.utils.experiments import Experiment
 from AutoEncodedFlows.datasets import Manifold1DDatasetNoise
-from AutoEncodedFlows.manifold_models import VectorFieldMasked
+from AutoEncodedFlows.manifold_models import VectorFieldMasked, VectorFieldTime
+from AutoEncodedFlows.manifold_models import CNFLearner
 from AutoEncodedFlows.utils.wandb_analysis import wandb_manifold1D_scatter_plot
-
-
-class CNFLearner(pl.LightningModule):
-    
-    def __init__(self, vector_field:nn.Module, dims:int):
-        """
-        Learner setup for Torchdyn CNF model
-
-        Parameters
-        ----------
-        model : nn.Module
-            AutoEncoder model with encode and decode methods
-
-        Returns
-        -------
-        None.
-
-        """
-        
-        super().__init__()
-        self.__name__ = 'CNFLearner'
-        self.iters = 0
-        self.dims = dims
-        self.losses = list()
-        
-        #Define model parameters:
-        ode_solver_args = {'solver':'tsit5'}
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        
-        #Create Jacobian noise dist and base dist:
-        self.mean = torch.zeros(self.dims).to(device)
-        self.cov = torch.eye(self.dims).to(device)
-        self.base_dist = MultivariateNormal(self.mean, self.cov)
-        
-        #Create model:
-        cnf = CNF(vector_field,
-                  noise_dist=self.base_dist,
-                  trace_estimator=hutch_trace)
-        node = NeuralODE(cnf, **ode_solver_args)
-        self.model = nn.Sequential(Augmenter(augment_idx=1, augment_dims=1),
-                                   node)
-        wandb.watch(self.model)
-        
-        
-    def forward(self, x):
-        
-        #Set model time span forward:
-        self.model[1].t_span = torch.linspace(0, 1, 10)
-        return self.model(x)
-    
-    def training_step(self, batch, batch_idx):
-        self.iters += 1 
-        t_eval, xtrJ = self.model(batch)
-        xtrJ = xtrJ[-1] #select the end point of the trajectory:
-        logprob = self.base_dist.log_prob(xtrJ[:,1:]).to(batch) - xtrJ[:,0]
-        loss = -torch.mean(logprob)
-        
-        if self.current_epoch % 10 == 0:
-            self.losses.append(loss.cpu().detach())
-        
-        #wandb logging:
-        wandb.log({'training loss': loss.detach().item(),
-                   'epoch': self.current_epoch})
-           
-        return {'loss': loss}   
-            
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=2e-3, weight_decay=1e-5)
-
-class VectorFieldNoTime(nn.Module):
-    
-    def __init__(self, dims, hidden_dims):
-        
-        super().__init__()
-        
-        self.__name__ = 'cnf_vector_field'
-        self.network = nn.Sequential(
-                           nn.Linear(dims, hidden_dims),
-                           nn.Tanh(),
-                           nn.Linear(hidden_dims, hidden_dims),
-                           nn.Tanh(),
-                           nn.Linear(hidden_dims, hidden_dims),
-                           nn.Tanh(),
-                           nn.Linear(hidden_dims, dims))
-    
-    def forward(self, x):
-        return self.network(x)
-    
-class VectorFieldTime(nn.Module):
-    
-    def __init__(self, dims, hidden_dims):
-        
-        super().__init__()
-        
-        self.__name__ ='cnf_vector_field_w_time'
-        self.network = nn.Sequential(
-                           DepthCat(1),
-                           nn.Linear(dims + 1, hidden_dims),
-                           nn.Tanh(),
-                           DepthCat(1),
-                           nn.Linear(hidden_dims + 1, hidden_dims),
-                           nn.Tanh(),
-                           DepthCat(1),
-                           nn.Linear(hidden_dims + 1, hidden_dims),
-                           nn.Tanh(),
-                           nn.Linear(hidden_dims, dims))
-        
-    def forward(self, x):
-        return self.network(x)
                 
                    
 if __name__ == '__main__':
@@ -148,7 +33,7 @@ if __name__ == '__main__':
                   'hidden_dims':64}
     dataset_args = {'n_samples':10_000,
                     'noise':0.15}
-    dataloader_args = {'batch_size':508,
+    dataloader_args = {'batch_size':256,
                        'shuffle':True}
     
     #Wrap multiple runs into Experiment Runner? -> probably
@@ -172,6 +57,7 @@ if __name__ == '__main__':
         #Try catch to ensure wandb.finish() is called:
         try:
             exp.run()
-            exp.wandb_analyse([wandb_manifold1D_scatter_plot])
+            with torch.no_grad():
+                exp.wandb_analyse([wandb_manifold1D_scatter_plot])
         finally:
             exp.finish()
